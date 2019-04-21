efd920ae8ea34f785445887be54cde4af39c7449
Jose Luis <jperegrina2@ucol.mx>
2019-04-21 16:50:26 -0500

    Style of the piano and the function click on all keys

--- piano/piano.js
+++ piano/piano.js
@@ -4,3 +4,13 @@
 
     We'll created a simple interactive piano.
 */
+window.addEventListener('load', ()=>{
+    const allKeys = document.querySelectorAll('.keyboard > .key');
+    
+    allKeys.forEach((key, index)=>{
+        key.addEventListener('click', function(){
+            console.log(this.innerHTML);
+        });
+    });
+    console.log('Are you ready, Pino Man?');
+});
