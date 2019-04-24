286b278e08c8bf52278cc443ceef8f88b89c0d10
Jose Luis <jperegrina2@ucol.mx>
2019-04-24 11:20:13 -0500

    keypress efect finished

--- piano/piano.js
+++ piano/piano.js
@@ -1,31 +1,51 @@
 'use strict'
 /*
     @author:José Luis Peregrina Rosales
+    @Others...
 
     We'll created a simple interactive piano.
 */
 window.addEventListener('load', ()=>{
     const allKeys = document.querySelectorAll('.keyboard > .key');
     this.addEventListener('keypress', function(e){
-        presionar(e.key.toLowerCase(), allKeys);
+        press(e.key.toLowerCase(), allKeys);
     });
 });
-function presionar(character, allKeys){
+function press(character, allKeys){
     switch(character){
-        case 'q':
+        case 'a':
+        case 's':
+        case 'd':
+        case 'f':
+        case 'h':
+        case 'j':
+        case 'k':
+        case 'l':
         case 'w':
-        case 'e':
         case 'r':
         case 't':
         case 'y':
-        case 'u':
-        case 'o':
-        case 'p':
-        playSound(character, allKeys);
+        case 'i':
+        findKey(character, allKeys);
         break;
     }
 }
 
-function playSound(e, allKeys){
-    console.log(allKeys);
+function findKey(e, allKeys){
+    debugger;
+    let arrayKeys = Array.from(allKeys);
+    let found = arrayKeys.find(function(key){
+        return key.id == e; 
+    });
+    pressEfect(found);
+}
+function pressEfect(element){
+    element.classList.add('keyPressed');
+    playSound(element);
+    setTimeout(()=>{
+        element.classList.remove('keyPressed');
+    }, 500);
+}
+function playSound(element){
+    
 }
